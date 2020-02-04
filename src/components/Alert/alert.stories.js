c898eb3e4cc201d2575704fdf98a520fc25f3683
johanahlin <johan.ahlin@lansforsakringar.se>
2020-02-04 10:22:57 +0100

    added new alert version

--- src/components/Alert/alert.stories.js
+++ src/components/Alert/alert.stories.js
@@ -1,6 +1,7 @@
 import Danger from "./danger.html";
 import Warning from "./warning.html";
 import Success from "./success.html";
+import Info from "./info.html";
 import WithButton from "./with-button.html";
 import WithCloseButton from "./with-close-button.html";
 
@@ -9,5 +10,6 @@ export default { title: 'Alert' }; // Component title
 export const danger = () => `${Danger}`;
 export const warning = () => `${Warning}`;
 export const success = () => `${Success}`;
+export const info = () => `${Info}`;
 export const withButton = () => `${WithButton}`;
 export const withCloseButton = () => `${WithCloseButton}`;
