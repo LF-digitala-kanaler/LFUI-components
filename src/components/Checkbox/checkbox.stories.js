48b875eafb3a2a8af294a201ea0b96da9a36d080
johanahlin <johan.ahlin@lansforsakringar.se>
2020-02-17 09:56:49 +0100

    added checkbox component

---
+++ src/components/Checkbox/checkbox.stories.js
@@ -0,0 +1,7 @@
+import Checkbox from "./checkbox.html";
+
+
+export default { title: 'Checkbox' }; 
+
+export const checkbox = () => `${Checkbox}`;
+
---
+++ src/components/DistributionBar/index.js
@@ -0,0 +1,9 @@
+import normal from "./default.html";
+import PointerSeparator from "./pointerSeparator.html";
+
+
+export default { title: 'Distribution Bar' }; 
+
+export const normal = () => `${Default}`;
+export const pointerSeparator = () => `${PointerSeparator}`;
+
