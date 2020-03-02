52f0ae0876619e4bc72f4edffd81fe369ee1783b
johanahlin <johan.ahlin@lansforsakringar.se>
2020-03-02 09:56:12 +0100

    added primary button example

--- src/docs/Avatar/avatar.stories.js
+++ src/docs/Avatar/avatar.stories.js
@@ -1,60 +1,13 @@
 import React from 'react';
 
+import colors from "./colors.html";
+import avatar from "./default.html";
+import sizes from "./sizes.html";
 
-export default {
-  title: 'Avatar',
-};
+export default { title: 'Avatar' }; 
 
-export const initials = () => (
-  <>
-  <div class="avatar avatar-blue avatar-inline" data-initials="JD">Jane Doe</div>
-  <div class="avatar avatar-blue avatar-inverted avatar-inline" data-initials="JD">Jane Doe</div>
-  </>
-);
-export const avatarWithImage = () => (
-  <div class="avatar avatar-white avatar-inline" data-initials="JD">Jane Doe</div>
-);
-
-export const white = () => (
-  <>
-    <div class="avatar avatar-white avatar-inline" data-initials="JD">Jane Doe</div>
-    <div class="avatar avatar-white avatar-inverted avatar-inline" data-initials="JD">Jane Doe</div>
-  </>
-);
-
-export const red = () => (
-  <>
-    <div class="avatar avatar-red avatar-inline" data-initials="JD">Jane Doe</div>
-    <div class="avatar avatar-red avatar-inverted avatar-inline" data-initials="JD">Jane Doe</div>
-  </>
-);
-
-export const gray = () => (
-  <>
-    <div class="avatar avatar-gray avatar-inline" data-initials="JD">Jane Doe</div>
-    <div class="avatar avatar-gray avatar-inverted avatar-inline" data-initials="JD">Jane Doe</div>
-  </>
-);
-
-export const black = () => (
-  <>
-    <div class="avatar avatar-black avatar-inline" data-initials="JD">Jane Doe</div>
-    <div class="avatar avatar-black avatar-inverted avatar-inline" data-initials="JD">Jane Doe</div>
-  </>
-);
-
-export const Sizes = () => (
-  <>
-    <div class="avatar avatar-lg avatar-inverted avatar-blue avatar-inline" data-initials="JD">
-      Jane Doe
-    </div>
-    <div class="avatar avatar-md avatar-inverted avatar-blue avatar-inline" data-initials="JD">
-      Jane Doe
-    </div>
-    <div class="avatar avatar-sm avatar-inverted avatar-blue avatar-inline" data-initials="JD">
-      Jane Doe
-    </div>
-  </>
-);
+export const normal = () => `${Default}`;
+export const pointerSeparator = () => `${PointerSeparator}`;
+export const tooltip = () => `${Tooltip}`;
 
 
