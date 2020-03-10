51e0239e60829b2636369d44c43838641788909b
johanahlin <johan.ahlin@lansforsakringar.se>
2020-03-10 14:11:48 +0100

    added dropdown component

--- src/docs/index.js
+++ src/docs/index.js
@@ -1,7 +1,9 @@
 import {disabledDatesExample,timepickerExample,datepickerExample} from './Datepicker/datepicker';
-
+import {selectExample} from './Dropdown/dropdown';
 
 // run your functions 
 disabledDatesExample();
 timepickerExample();
 datepickerExample();
+
+selectExample();
