ac75297268df0d0ec484cf72bff14b9c17c67950
johanahlin <johan.ahlin@lansforsakringar.se>
2020-03-11 19:36:40 +0100

    updated radio card component

--- src/docs/RadioButtons/radioButtons.stories.js
+++ src/docs/RadioButtons/radioButtons.stories.js
@@ -1,17 +1,12 @@
-import Standard from "./standard.html";
-import Stretched from "./stretched.html";
+import ButtonGroup from "./buttonGroup.html";
 import RadioButton from "./radioButton.html";
 
 import RadioCards from "./radioCards.html";
 import RadioCardSmall from "./radioCardSmall.html";
-import RadioCardSmallInvalid from "./radioCardSmallInvalid.html";
 
-export default { title: 'Radio Buttons' }; // Component title
+export default { title: 'Radio Buttons' }; 
 
 export const radioButton = () => `${RadioButton}`;
-
-export const standard = () => `${Standard}`;
-export const stretched = () => `${Stretched}`;
+export const buttonGroup = () => `${ButtonGroup}`;
 export const radioCards = () => `${RadioCards}`;
 export const radioCardSmall = () => `${RadioCardSmall}`;
-export const radioCardSmallInvalid = () => `${RadioCardSmallInvalid}`;
