4ae8f29c20341e288e43aaf9d756dbe37f3d8e78
johanahlin <johan.ahlin@lansforsakringar.se>
2020-03-23 10:13:41 +0100

    added links components

---
+++ src/docs/Links/links.stories.js
@@ -0,0 +1,7 @@
+import Links from "./links.html";
+import List from "./linkList.html";
+
+export default { title: 'Links' }; 
+
+export const links = () => `${Links}`;
+export const list = () => `${List}`;
