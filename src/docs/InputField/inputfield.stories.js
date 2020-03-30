62be3e9a8de98f7faca2a51183e7487cb01f1a3c
johanahlin <johan.ahlin@lansforsakringar.se>
2020-03-30 20:43:36 +0200

    added new examples

--- src/docs/InputField/inputfield.stories.js
+++ src/docs/InputField/inputfield.stories.js
@@ -1,6 +1,7 @@
-import Inputfield from "./inputfield.html";
-
+import OneLined from "./oneLined.html";
+import SeveralLines from "./severalLines.html";
 
 export default { title: 'Input Field' }; 
 
-export const inputfield = () => `${Inputfield}`;
+export const oneLined = () => `${OneLined}`;
+export const severalLines = () => `${SeveralLines}`;
--- src/docs/RadioButtons/radioButtons.stories.js
+++ src/docs/RadioButtons/radioButtons.stories.js
@@ -1,12 +1,12 @@
-import ButtonGroup from "./buttonGroup.html";
-import RadioButton from "./radioButton.html";
 
+import StyledRadioButtons from "./styledRadioButtons.html";
+
+import ButtonGroup from "./buttonGroup.html";
 import RadioCards from "./radioCards.html";
-import RadioCardSmall from "./radioCardSmall.html";
+
 
 export default { title: 'Radio Buttons' }; 
 
-export const radioButton = () => `${RadioButton}`;
+export const styledRadioButtons = () => `${StyledRadioButtons}`;
 export const buttonGroup = () => `${ButtonGroup}`;
 export const radioCards = () => `${RadioCards}`;
-export const radioCardSmall = () => `${RadioCardSmall}`;
