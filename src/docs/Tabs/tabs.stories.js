3f820e0493488e3b5eea597e2b6973c5ec490259
johanahlin <johan.ahlin@lansforsakringar.se>
2020-04-02 12:52:11 +0200

    added tabs

---
+++ src/docs/Tabs/tabs.stories.js
@@ -0,0 +1,9 @@
+import InPage from "./In-pageNavigation.html";
+import PageLevel from "./Page-levelNavigation.html";
+
+export default { title: 'Tabs' }; 
+
+export const inPage = () => `${InPage}`;
+export const pageLevel = () => `${PageLevel}`;
+
+
