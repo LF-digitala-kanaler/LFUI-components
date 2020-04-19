427c57ed8d1167fe755fcb88ffab032f95ee68e7
johanahlin <johan.ahlin@lansforsakringar.se>
2020-04-19 12:01:09 +0200

    added new template for icons

--- src/icon/build.js
+++ src/icon/build.js
@@ -10,25 +10,25 @@ const argv = require('minimist')(process.argv.slice(2));
 
 const BASE_ICON_DIR = './src/svg';
 const ICON_DIRS = [
-    { directory: '10px', width: 10, height: 10, header: '10 px icons'},
-    { directory: '20px', width: 20, height: 20, header: '20 px icons'},
-    { directory: '20px-color', width: 20, height: 20, header: '20 px color icons'},
-    { directory: '24px', width: 24, height: 24, header: '24 px icons'},
-    { directory: '24px-color', width: 24, height: 24, header: '24 px color icons'},
-    { directory: '26px', width: 26, height: 26, header: '26 px icons' },
-    { directory: '32px', width: 32, height: 32, header: '32 px icons' },
-    { directory: '32px-color', width: 32, height: 32, header: '32 px color icons' },
-    { directory: '40px', width: 40, height: 40, header: '40 px icons' },
-    { directory: '40px-color', width: 40, height: 40, header: '40 px color icons' },
-    { directory: '48px-color', width: 48, height: 48, header: '48 px color icons' },
-    { directory: '50px-color', width: 50, height: 50, header: '50 px color icons' },
-    { directory: '70x50px', width: 70, height: 50, header: '70x50 px color icons' },
-    { directory: '56px', width: 56, height: 56, header: '56 px color icons' },
-    { directory: '64px-color', width: 64, height: 64, header: '64 px color icons' },
-    { directory: '70px', width: 70, height: 70, header: '70 px icons' },
-    { directory: '70px-color', width: 70, height: 70, header: '70 px color icons' },
-    { directory: '84x50px-color', width: 84, height: 50, header: '84x50  px color icons' },
-    { directory: 'mega-menu-icons', width: 32, height: 32, header: 'Mega menu icons' }
+    { directory: '10px', width: 10, height: 10, header: '10 pixels icons', special: true },
+    { directory: '20px', width: 20, height: 20, header: '20 pixels icons', special: false},
+    { directory: '20px-color', width: 20, height: 20, header: '20 pixels color icons', special: false},
+    { directory: '24px', width: 24, height: 24, header: '24 pixels icons', special: false},
+    { directory: '24px-color', width: 24, height: 24, header: '24 pixels color icons', special: false},
+    { directory: '26px', width: 26, height: 26, header: '26 pixels icons', special: true  },
+    { directory: '32px', width: 32, height: 32, header: '32 pixels icons' , special: false},
+    { directory: '32px-color', width: 32, height: 32, header: '32 pixels color icons' , special: false},
+    { directory: '40px', width: 40, height: 40, header: '40 pixels icons' , special: false},
+    { directory: '40px-color', width: 40, height: 40, header: '40 pixels color icons' , special: false},
+    { directory: '48px-color', width: 48, height: 48, header: '48 pixels color icons' , special: false},
+    { directory: '50px-color', width: 50, height: 50, header: '50 pixels color icons', special: false },
+    { directory: '70x50px', width: 70, height: 50, header: '70x50 pixels color icons' , special: false},
+    { directory: '56px', width: 56, height: 56, header: '56 pixels color icons', special: false },
+    { directory: '64px-color', width: 64, height: 64, header: '64 pixels color icons', special: false },
+    { directory: '70px', width: 70, height: 70, header: '70 pixels icons', special: false },
+    { directory: '70px-color', width: 70, height: 70, header: '70 pixels color icons', special: false },
+    { directory: '84x50px-color', width: 84, height: 50, header: '84x50  pixels color icons', special: false },
+    { directory: '84x50px-color', width: 84, height: 50, header: '84x50  pixels color icons', special: false },
 ];
 
 const spriter = new SVGSpriter({
@@ -44,8 +44,9 @@ const spriter = new SVGSpriter({
 
 var iconDirs = [];
 
-ICON_DIRS.forEach(dir => {
-  iconDirs.push({ directory: dir.directory, width: dir.width, height: dir.height, header: dir.header, files: fs.readdirSync(BASE_ICON_DIR + '/' + dir.directory), icons: []});
+
+ICON_DIRS.map(dir => {
+  iconDirs.push({special: dir.special, directory: dir.directory, width: dir.width, height: dir.height, header: dir.header, files: fs.readdirSync(BASE_ICON_DIR + '/' + dir.directory), icons: []});
 });
 
 iconDirs.forEach(iconDir => {
@@ -54,11 +55,22 @@ iconDirs.forEach(iconDir => {
   });
 });
 
-fs.writeFileSync(
-  path.resolve(__dirname, 'README.md'),
-  templates['README.md'](iconDirs)
-);
 
+// split into regular and special icons 
+let iconsDirPresentation = {regular:[], special:[]}
+iconDirs.forEach(item => item.special == false ? iconsDirPresentation.regular.push(item) : iconsDirPresentation.special.push(item))
+
+console.log(iconsDirPresentation)
+
+
+fs.writeFileSync(
+  path.resolve(__dirname, 'Regular.md'),
+  templates['Regular.md'](iconsDirPresentation.regular )
+);
+fs.writeFileSync(
+  path.resolve(__dirname, 'Special.md'),
+  templates['Special.md'](iconsDirPresentation.special )
+);
 iconDirs.forEach(iconDir => {
   iconDir.files.forEach(file => {
     const filePath = path.resolve(BASE_ICON_DIR + '/' + iconDir.directory, file);
