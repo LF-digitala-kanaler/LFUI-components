d06ef990b06fbba56b8172abd12de09bf12560e8
johanahlin <johan.ahlin@lansforsakringar.se>
2020-04-27 00:23:56 +0200

    added loader for inline svg

--- webpack.config.js
+++ webpack.config.js
@@ -140,7 +140,12 @@ module.exports = {
             loader: 'postcss-loader',
             options: {
               plugins: () => [
+                require('postcss-base64')({
+                  pattern: /<svg.*<\/svg>/i,
+                  prepend: 'data:image/svg+xml;base64,'
+                }),
                 increaseSpecificity({repeat:1, stackableRoot: '.lfui-theme' }),
+                
               ],
             }
           },
