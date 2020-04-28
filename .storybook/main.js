ffeb7884fb48dbf93a55e3808953c8c47aaac21b
johanahlin <johan.ahlin@lansforsakringar.se>
2020-04-28 20:29:30 +0200

    fixed focus state on docs

--- .storybook/main.js
+++ .storybook/main.js
@@ -10,29 +10,3 @@ addons: [
 ]
 };
 
-
-// import '@storybook/addon-links/register';
-
-
-
-// import '../src/scss/LFUI.scss';
-
-// import '../node_modules/eonasdan-bootstrap-datetimepicker'
-// import '../src/js/index.js';
-// import theme from './theme';
-// import { addDecorator, configure, addParameters } from '@storybook/html';
-
-
-
-// addDecorator(withA11y)
-
-// addParameters({
-//   backgrounds: [
-//     { name: 'White', value: '#ffffff', default: true },
-//     { name: 'Gray', value: '#222222' },
-//   ],
-//   options: {
-//     theme: theme,
-//   },
-// });
-
