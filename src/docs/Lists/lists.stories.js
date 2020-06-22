3db11930aef5740a9fe19d336691b30a40f8abb4
johan ahlin <johan.ahlin@lansforsakringar.se>
2020-06-22 10:06:41 +0200

    re-arranged comps

--- src/docs/Lists/lists.stories.js
+++ src/docs/Lists/lists.stories.js
@@ -1,9 +1,11 @@
 import Dots from "./dots.html";
 import Checkmarks from "./checkmarks.html";
 import Numbers from "./numbers.html";
+import LinkLists from "./linkLists.html";
 export default { title: 'Lists' }; 
 
 export const numbers = () => `${Numbers}`;
 export const checkmarks = () => `${Checkmarks}`;
 export const dots = () => `${Dots}`;
+export const linkLists = () => `${LinkLists}`;
 
