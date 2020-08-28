f6dde2ecf43d1cc5fe9cd739241a7851b583926a
johan ahlin <johan.ahlin@lansforsakringar.se>
2020-08-28 13:04:32 +0200

    fixed testing

--- src/test/storyshots.test.js
+++ src/test/storyshots.test.js
@@ -1,12 +1,58 @@
 import initStoryshots from '@storybook/addon-storyshots';
 import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer';
-import path from 'path';
+
+const getMatchOptions = ({ context: { kind, story }, url }) => {
+  return {
+    failureThreshold: 0.2,
+    failureThresholdType: 'percent',
+  };
+};
+
+const beforeScreenshot = async page => {
+  
+  await page.evaluate(() => {
+    document.querySelectorAll('animate').forEach(el => {
+      el.setAttribute('repeatCount', 'indefinite');
+      el.setAttribute('dur', 'indefinite');
+    });
+  });
+
+  // Fixing the Animation by inlining, previous approach with external file was flaky for the animation
+  page.addStyleTag({
+    content: `
+    *, *::after, *::before {
+      -webkit-transition: none !important;
+      -moz-transition: none !important;
+      -ms-transition: none !important;
+      -o-transition: none !important;
+      transition: none !important;
+  
+      -webkit-animation: none !important;
+      -moz-animation: none !important;
+      -ms-animation: none !important;
+      -o-animation: none !important;
+      animation: none !important;
+    }`
+  
+  });
+  return new Promise(resolve =>
+    setTimeout(() => {
+      resolve();
+    }, 600)
+  );
+  
+  
+};
+
 
 
 initStoryshots({
   suite: 'Image storyshots',
   framework: 'html',
   test: imageSnapshot({
-    storybookUrl: 'http://localhost:6006'
+    storybookUrl: 'http://localhost:6006',
+    beforeScreenshot,
+    getMatchOptions,
+
   }),
 });
