950b56f0097d6d743aa1d51f37d24dc86a79de35
johan ahlin <johan.ahlin@lansforsakringar.se>
2020-09-18 15:29:33 +0200

    updated lang on dropdowns

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # LFUI
 
+Copy of the offical 6.6.0-branch. You can find the orignal one [here](https://github.com/LF-digitala-kanaler/LFUI) 
+
 LFUI is a HTML-, CSS- and JS framework built on top of Bootstrap to be used in Länsförsäkringar's different applications and websites. LFUI has restyled many of Bootstrap's native components as well as included new ones specific for Länsförsäkringar, along with other custom features and utilities.
 
 
-For information how to use LFUI 6.5.0 visit the old [lfui](https://github.com/LF-digitala-kanaler/LFUI) repository.
+
 
 ## Install
 
--- src/docs/Dropdown/dropdown.js
+++ src/docs/Dropdown/dropdown.js
@@ -1,10 +1,10 @@
 import CustomSelect from '../../js/components/custom-select.js';
 
 const dropdownExample = () => {
-  new CustomSelect( $('#custom-select') );
-  new CustomSelect( $('#custom-select2') );
-  new CustomSelect( $('#custom-select3') );
-  new CustomSelect( $('#custom-select4') );
+  new CustomSelect( $('#custom-select'));
+  new CustomSelect( $('#custom-select2'));
+  new CustomSelect( $('#custom-select3'));
+  new CustomSelect( $('#custom-select4'));
   new CustomSelect( $('#custom-multi-select-1') );
   new CustomSelect( $('#custom-multi-select-2') );
   new CustomSelect( $('#custom-multi-select-3') );
