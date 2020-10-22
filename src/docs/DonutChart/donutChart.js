86d8903cd5d8865e18d910c151578f8e5b7036bc
johanahlin <johan.ahlin@lansforsakringar.se>
2020-10-22 14:39:50 +0200

    added donut chart

--- src/docs/DonutChart/donutChart.js
+++ src/docs/DonutChart/donutChart.js
@@ -3,9 +3,11 @@ let filter = require('lodash/filter');
 let isEmpty = require('lodash/isEmpty');
 let remove = require('lodash/remove');
 
-const legends = () => {
-
+window.highlightArcWithIndex = (index) => {
+  
+  highlight(null, [], index);
 }
+
 const donutChartExample = () => {
   const ctx = document.getElementById('donutChart').getContext('2d');
   
@@ -13,25 +15,40 @@ const donutChartExample = () => {
   const colors = chartColors.getColorScale(data.length);
   const disabledColor = chartColors.getDisabledColor();
   const highlightedArc = [];
+  let legendStyles = [];
   let firstClick = true
-  
+ 
   const _ = {
     filter: filter,
     isEmpty: isEmpty,
     remove: remove
   };
-
+  const legend = (index) => {
+    
+    data.forEach((data, index) => {
+      legendStyles.push({
+        backgroundColor: !_.isEmpty(highlightedArc) && !isHighlighted(index)
+          ? chartColors.getDisabledColor()
+          : colors[index]
+      });
+    })
+  }
+  console.log(legend(), 'leg')
   const isHighlighted = (index) => {
     return !_.isEmpty(_.filter(highlightedArc, (arc) => {
       return arc === index;
     }));
   }
 
-  const highlight = (evt, elements, index) => {
-
+  window.highlight = (evt, elements, index) => {
+    legendStyles = [];
     let dataset = donutChart.data.datasets[0];
-    index = elements[0]._index;
-    // om första klick eller denna är inte highlighad
+    if (elements.length) {
+      index = elements[0]._index;
+    } else if (typeof index === 'undefined') {
+      return;
+    }
+    
     if(firstClick || !isHighlighted(index)) {
       highlightedArc.push(index);
       firstClick = false;
@@ -48,13 +65,17 @@ const donutChartExample = () => {
     dataset.hoverBackgroundColor = colors.slice();
     
     for (let i = 0; i < dataset.backgroundColor.length; i++) {
-      console.log(!isHighlighted(i), i, 'sss')
       if (!isHighlighted(i)) {
         dataset.hoverBackgroundColor[i] = disabledColor;
         dataset.backgroundColor[i] = disabledColor;
       }
     }
+    
+    document.getElementById('js-chartLegends').innerHTML = '';
+    document.getElementById('js-chartLegends').innerHTML = donutChart.generateLegend();
     donutChart.update()
+    
+    
   }
 
   const donutChart = new Chart(ctx, {
@@ -78,18 +99,22 @@ const donutChartExample = () => {
         enabled: false
       },
       legend: false,
-      legendCallback: function(chart) {
+      legendCallback: function(index) {
+        legend(index)
         var text = [];
-          for (var i = 0; i < chart.data.datasets.length; i++) {
-              text.push('<li data-index="index-'+i+'" class="chart-legend-item chart-legend-item-clickable" onclick="updateDataset(event, ' + '\'' + i + '\'' + ')"><div class="chart-legend-box"  style="background-color:' + colors[i] + '"></div>'+ "Länsförsäkringar Dummy fond " + (i+1) +'</li>');
+          for (var i = 0; i < donutChart.data.datasets[0].data.length; i++) {
+              text.push('<li data-index="index-'+i+'" class="chart-legend-item chart-legend-item-clickable" onclick="highlightArcWithIndex('+i+')"><div class="chart-legend-box"  style="background-color:' + legendStyles[i].backgroundColor + '"></div>'+ "Länsförsäkringar Dummy fond " + (i+1) +'</li>');
           }
           return text.join("");   
       },
       onClick: highlight,
       maintainAspectRatio: false,
     }
-    
+
   })
+  
   document.getElementById('js-chartLegends').insertAdjacentHTML('beforeend', donutChart.generateLegend());
+
+  
 }
 export {donutChartExample}
