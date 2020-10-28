9cbf693fe215f7148f8c02fb74b7d277f5afac0b
johanahlin <johan.ahlin@lansforsakringar.se>
2020-10-28 10:40:47 +0100

    added map examle

--- src/docs/MapChart/mapChart.js
+++ src/docs/MapChart/mapChart.js
@@ -1,60 +1,85 @@
 
- import chartColors from '../../data/chartColors';
+import chartColors from '../../data/chartColors';
 
 const mapChartExample = () => {
-
-  
-
+  let countryList = [{country: 'Sweden',value:10},{country:'Germany',value:15},{country:'France',value:8}, {country:'Norway',value:8}];
 
   Promise.all([
     fetch('https://unpkg.com/world-atlas/countries-50m.json').then((r) => r.json())
     ]).then(([data]) => {
-    const countries = ChartGeo.topojson.feature(data, data.objects.countries).features.filter((f) => f.properties.name !== 'Antarctica');
-    let countryList = [{country: 'Sweden',value:10},{country:'Germany',value:15},{country:'France',value:8}];
-    const lookup = new Map(countryList.map((d) => [d.country, d.value]));
     
+    const countries = ChartGeo.topojson.feature(data, data.objects.countries).features.filter((f) => f.properties.name !== 'Antarctica');
+    const lookup = new Map(countryList.map((d) => [d.country, d.value]));
+    const colors = chartColors.getColorScale(countryList.length);
+    
+    let count = 0;
+    
+    const selected = countries.map((item) => { 
+      if(lookup.get(item.properties.name)){
+        count++
+        return colors[count-1]
+      }else{
+        return chartColors.getDisabledColor();
+      }
+    })
+
     const display_list = countries.map((element) => {
-      
-      
       return {
           feature: element,
           value: lookup.get(element.properties.name) || 0,
        };
      });
-     console.log(display_list)
-    const chart = new Chart(document.getElementById("mapChart").getContext("2d"), {
+    
+   
+    const mapChart = new Chart(document.getElementById("mapChart").getContext("2d"), {
       type: 'choropleth',
       data: {
         datasets: [{
-          label: 'Countries',
           data: display_list,
-          
+          hoverBackgroundColor: selected,
+          backgroundColor: selected
         }],
         
       },
       
       options: {
-        // tooltips: {
-        //   enabled: false
-        // },
+        animation: {
+          duration: 0
+        },
+        tooltips: {
+          enabled: false
+        },
         legend: {
           display: false
+        },
+        legendCallback: function (mapChart) {  
+       
+          var text = [];
+          for (var i = 0; i < countryList.length; i++) {
+              text.push('<li  class="chart-legend-item"><div class="chart-legend-box"  style="background-color:' + colors[i] + '"></div>'+ countryList[i].country +'</li>');
+          }
+          return text.join("");   
         },
         scale: {
           projection: 'mercator'
         },
         geo: {
-          
+        
         },
         elements: {
           geoFeature: {
             
-          }
+            borderColor: chartColors.getDisabledColor(),
+          
+          },
+         
         }
         
       }
+      
     });
-    console.log(chart.config.options)
+    document.getElementById('js-chartLegends').insertAdjacentHTML('beforeend', mapChart.generateLegend());
+    console.log(mapChart.config.options.events)
   });
   
 }
