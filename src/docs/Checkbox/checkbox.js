800789cc6456829a3414865581edad7d8ea2c9b6
johanahlin <johan.ahlin@lansforsakringar.se>
2020-12-07 16:40:32 +0100

    fixed checkbox example

--- src/docs/Checkbox/checkbox.js
+++ src/docs/Checkbox/checkbox.js
@@ -1,18 +1,18 @@
 
 const checkboxExample = () => {
 
-let checkboxCards = document.getElementsByClassName('checkbox-card');
+  let checkboxCards = document.getElementsByClassName('checkbox-card');
+  if(checkboxCards.length){
+      for(let i = 0; i < checkboxCards.length; i++) {
+        checkboxCards[i].addEventListener('change', function(){
 
-    for(let i = 0; i < checkboxCards.length; i++) {
-      checkboxCards[i].addEventListener('change', function(){
-
-        if(!this.classList.contains('active')) {
-          this.classList.add('active');
-        }else{
-          this.classList.remove('active');
-        }
-      });
-    }
+          if(!this.classList.contains('active')) {
+            this.classList.add('active');
+          }else{
+            this.classList.remove('active');
+          }
+        });
+      }
+  }
 }
-
 export {checkboxExample}
