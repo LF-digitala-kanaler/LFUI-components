fd7e2e13faa23a495d864c721c2e5892f2242892
johanahlin <johan.ahlin@lansforsakringar.se>
2020-12-14 15:22:01 +0100

    updated docs from 6.6.0

--- src/docs/LeadParagraph/leadParagraph.stories.js
+++ src/docs/LeadParagraph/leadParagraph.stories.js
@@ -1,5 +1,6 @@
 import LeadParagraph from "./leadParagraph.html";
-
+import PayOff from "./payOff.html";
 export default { title: 'Lead Paragraph' }; 
 
 export const leadParagraph = () => `${LeadParagraph}`;
+export const payOff = () => `${PayOff}`;
---
+++ src/docs/RadioButtons/radioButtons.js
@@ -0,0 +1,17 @@
+  
+const radioButtonsExample = () => {
+  let radioCards = document.getElementsByClassName('radio-card');
+    for(let i = 0; i < radioCards.length; i++) {
+      radioCards[i].addEventListener('change', function(){
+        for(let i = 0; i < radioCards.length; i++) {
+          radioCards[i].classList.remove('active');
+        }
+
+        if(!this.classList.contains('active')) {
+          this.classList.add('active');
+        }
+      });
+    }
+}
+
+export {radioButtonsExample}
