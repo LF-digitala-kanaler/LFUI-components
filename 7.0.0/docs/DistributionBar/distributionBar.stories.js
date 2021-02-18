e54c131c88bd9667bd373fdd38766027ad00cdb9
johanahlin <42958506+johanahlin@users.noreply.github.com>
2021-02-18 08:36:35 +0000

    Deploying to gh-pages from  @ da498695b948d9fd5ae44e4a43a518826e8f50e0 🚀

--- 7.0.0/docs/DistributionBar/distributionBar.stories.js
+++
@@ -1,9 +0,0 @@
-import DistributionBar from "./default.html";
-import ProgressBar from "./progressBar.html";
-
-
-export default { title: 'Distribution Bar' }; 
-
-export const distributionBar = () => `${DistributionBar}`;
-export const progressBar = () => `${ProgressBar}`;
-
