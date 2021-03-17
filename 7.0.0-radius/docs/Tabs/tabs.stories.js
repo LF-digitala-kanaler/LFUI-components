0362163a06cf1a6b759f00023eb566ec35bc316e
johanahlin <42958506+johanahlin@users.noreply.github.com>
2021-03-17 11:05:29 +0000

    Deploying to gh-pages from  @ 060c8c5975b2204905bb711f4f7b455b5ec2b207 🚀

--- 7.0.0-radius/docs/Tabs/tabs.stories.js
+++
@@ -1,9 +0,0 @@
-import InPage from "./InPageNavigation.html";
-import PageLevel from "./PageLevelNavigation.html";
-
-export default { title: 'Tabs' }; 
-
-export const inPage = () => `${InPage}`;
-export const pageLevel = () => `${PageLevel}`;
-
-
