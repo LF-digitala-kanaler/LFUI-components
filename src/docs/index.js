1ec27c385788fa5f6cd6da0b57328d389634d1c3
johanahlin <johan.ahlin@lansforsakringar.se>
2021-09-14 15:39:10 +0200

    docs: change dropdown example name

--- src/docs/index.js
+++ src/docs/index.js
@@ -1,26 +1,29 @@
-import {disabledDatesExample,timepickerExample,datepickerExample} from './Datepicker/datepicker';
-import {selectExample} from './Dropdown/dropdown';
-import {calloutExample} from './Callout/callout';
-import {priceBoxExample} from './PriceBox/pricebox';
-import {sliderExample} from './Slider/slider';
-import {panelExample} from './Panel/panel';
-import {uploadExample} from './Upload/upload';
-import {tableExample} from './Tables/table'
-import {timelineExample} from './Timeline/timeline';
-import {navbarExample} from './Navbar/navbar';
-import {popoverTooltipExample} from './PopoverTooltip/popovertooltip'
-import {valueboxExample} from './ValueBox/valuebox';
-import {barChartExample} from './BarChart/barChart';
-import {mapChartExample} from './MapChart/mapChart';
-import {lineChartExample} from './LineChart/lineChart';
-import {donutChartExample} from './DonutChart/donutChart';
-import {xyPlotExample} from './xyPlot/xyPlot';
+import '../scss/DOCS.scss';
+
+import { datepickerExample, disabledDatesExample, timepickerExample } from './Datepicker/datepicker';
+
+import { barChartExample } from './BarChart/barChart';
+import { calloutExample } from './Callout/callout';
+import { donutChartExample } from './DonutChart/donutChart';
+import { dropdownExample } from './Dropdown/dropdown';
+import { lineChartExample } from './LineChart/lineChart';
+import { mapChartExample } from './MapChart/mapChart';
+import { navbarExample } from './Navbar/navbar';
+import { panelExample } from './Panel/panel';
+import { popoverTooltipExample } from './PopoverTooltip/popovertooltip'
+import { priceBoxExample } from './PriceBox/pricebox';
+import { sliderExample } from './Slider/slider';
+import { tableExample } from './Tables/table'
+import { timelineExample } from './Timeline/timeline';
+import { uploadExample } from './Upload/upload';
+import { valueboxExample } from './ValueBox/valuebox';
+import { xyPlotExample } from './xyPlot/xyPlot';
 // run your functions 
 disabledDatesExample();
 timepickerExample();
 datepickerExample();
 tableExample();
-selectExample();
+dropdownExample();
 
 calloutExample();
 
@@ -43,4 +46,3 @@ valueboxExample();
 popoverTooltipExample();
 
 
-import '../scss/DOCS.scss';
