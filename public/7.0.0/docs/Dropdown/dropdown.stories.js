6de370e235371984c3a82b48f8bcb5778a18058f
johanahlin <johan.ahlin@lansforsakringar.se>
2021-09-20 10:42:36 +0200

    remove listeners to dropdown

--- public/7.0.0/docs/Dropdown/dropdown.stories.js
+++ public/7.0.0/docs/Dropdown/dropdown.stories.js
@@ -1,24 +1,23 @@
-
-
-import SelectOne from "./selectOne.html";
 import MultiSelect from "./multiSelect.html";
-import {dropdownExample} from "./dropdown"
+import SelectOne from "./selectOne.html";
+import { dropdownExample } from "./dropdown"
 import { useEffect } from "@storybook/client-api";
 
-export default { title: 'Dropdown' }; 
+export default { title: 'Dropdown' };
 
 export const selectOne = () => {
   useEffect(() => {
     dropdownExample()
   }, []);
   return SelectOne
-  
+
 };
 
 export const multiSelect = () => {
   useEffect(() => {
     dropdownExample()
+
   }, []);
   return MultiSelect
-  
+
 };
