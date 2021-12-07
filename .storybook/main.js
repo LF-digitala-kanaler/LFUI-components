761e321140479ccecb6330f967087b16a54e0f8d
johanahlin <johan.ahlin@lansforsakringar.se>
2021-12-07 10:24:45 +0100

    chore: update dependencies

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,62 +1,62 @@
 const path = require('path');
-
 module.exports = {
-stories: ['../src/**/*.stories.[tj]s'],
-addons: [
-  '@storybook/addon-a11y',
-  '@storybook/addon-backgrounds',
-  '@storybook/addon-viewport',
-  '@storybook/addon-docs'
-],
 
-webpackFinal: async (config, { configType }) => {
-  // override svg so icons will work in prod
-    console.log(configType)
+  stories: ['../src/**/*.stories.[tj]s'],
+  staticDirs: ['../src'],
+  addons: [
+    '@storybook/addon-a11y',
+    '@storybook/addon-backgrounds',
+    '@storybook/addon-viewport',
+    '@storybook/addon-docs'
+  ],
+  webpackFinal: async (config, { configType }) => {
+
     config.module.rules = config.module.rules.map(rule => {
-      if (rule.test.toString().includes('svg')) {
+      if (rule.test && rule.test.toString().includes('svg')) {
         const test = rule.test.toString().replace('svg|', '').replace(/\//g, '')
         return { ...rule, test: new RegExp(test) }
       } else {
         return rule
       }
     });
-  
-  config.module.rules.push(
-    {
-      test: /\.svg$/,
-      use: [
-        {
-          loader: 'file-loader',
-          options: {
-            name: '[path][name].[ext]'
-          },
-        },
-      ],
-    },
-    {
-      test: /\.(sa|sc|c)ss$/,
-      use: [
-        {
-          loader: "style-loader",
-        },
-        {
-          loader: 'css-loader',
-          options: {
-              importLoaders: 1
-          },
-        },
-        'postcss-loader',
-        {
-          loader: 'sass-loader',
-          options: {
-            sourceMap: true,
-          },
-        },
-      ],
-      
-    }
-  );
 
-  // Return the altered config
-  return config;
-}};
+    config.module.rules.push(
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]'
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1
+            },
+          },
+          'postcss-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+
+      }
+    );
+
+    // Return the altered config
+    return config;
+  }
+};
