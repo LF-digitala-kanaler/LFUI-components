b4ea7eeb47050d089b5c2cef240a8f284adbc909
johanahlin <johan.ahlin@lansforsakringar.se>
2021-12-07 17:27:10 +0100

    docs: changed base path to fix icons in prod site

--- public/7.1.0/docs/Links/links.stories.js
+++
@@ -1,7 +0,0 @@
-import Primary from "./primary.html";
-import Regular from "./regular.html";
-
-export default { title: 'Links' }; 
-
-export const primary = () => `${Primary}`;
-export const regular = () => `${Regular}`;
--- public/7.1.0/js/components/datepicker.js
+++
@@ -1,4 +0,0 @@
-import moment from 'moment';
-import 'moment/locale/sv';
-moment.locale('sv');
-import 'eonasdan-custom-bootstrap-datetimepicker-with-bootstrap-4';
