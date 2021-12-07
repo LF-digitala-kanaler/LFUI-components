8245b779c3eb8e2b76bc2ff13d383b2302833df2
johanahlin <42958506+johanahlin@users.noreply.github.com>
2021-12-07 13:25:59 +0000

    Deploying to gh-pages from  @ 33849d24b30932b38645afda68e134a896e498bd 🚀

--- 7.0.1/docs/Pagination/pagination.js
+++
@@ -1,9 +0,0 @@
-const paginationExample = () => {
-  $('.pagination a').not('.pagination-static a').click(function() {
-    event.preventDefault();
-    $('.pagination .page-item').not('.pagination-static .page-item').removeClass('active')
-    $(this).parent().addClass('active')
-  });
-}
-
-export {paginationExample}
