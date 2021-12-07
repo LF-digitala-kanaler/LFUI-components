a2c551167d6660646ae2095e2fc94a4ce5dd1cc0
johanahlin <johan.ahlin@lansforsakringar.se>
2021-12-07 10:24:45 +0100

    chore: update dependencies

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,15 +1,14 @@
-
 import '../src/scss/LFUI.scss';
 import 'eonasdan-custom-bootstrap-datetimepicker-with-bootstrap-4';
 import '../src/js/index.js';
 
 export const parameters = {
   backgrounds: {
-  default: 'white',
-  values: [
-    { name: 'white', value: '#ffffff' },
-    { name: 'grey', value: '#F3F3F3' },
-    {name: 'light blue', value: '#E5EFF7'}
+    default: 'white',
+    values: [
+      { name: 'white', value: '#ffffff' },
+      { name: 'grey', value: '#F3F3F3' },
+      { name: 'light blue', value: '#E5EFF7' }
     ],
   }
 }
