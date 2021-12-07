21465676522b4252cb3ef8034ab1d65851b5cce7
johanahlin <johan.ahlin@lansforsakringar.se>
2021-12-08 00:07:57 +0100

    docs: changed base path to fix icons in prod site

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,7 @@ const path = require('path');
 module.exports = {
   logLevel: 'debug',
   stories: ['../src/**/*.stories.[tj]s'],
-  staticDirs: [{ from: './src/icons/', to: '../static/icons' }],
+  staticDirs: ['./src/icons'],
   addons: [
     '@storybook/addon-a11y',
     '@storybook/addon-backgrounds',
