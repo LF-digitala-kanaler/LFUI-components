220dc305919b96450ab1944d73028a3221239c80
Carl Törnqvist <carl@tornqv.ist>
2021-12-21 13:17:09 +0000

    Use custom property for controlling slider

--- src/docs/Slider/slider.js
+++ src/docs/Slider/slider.js
@@ -19,48 +19,41 @@ const sliderExample = () => {
   $('#year-slider-range-to').change(function () {
     $('#year-slider-range').slider('values', 1, $('#year-slider-range-to').val())
   })
+
   $('#lf-slider-1').on('input', function () {
     const percent = Math.ceil(((this.value - this.min) / (this.max - this.min)) * 100)
-    $(this).css('background', 'linear-gradient(to right, #005aa0 0%, #005aa0 ' + percent + '%, #d9d9d9 ' + percent + '%)')
-
+    this.style.setProperty('--progress', percent)
     $('#lf-slider-value').val(this.value)
   })
 
   $('#lf-slider-value').on('input', function () {
     $('#lf-slider-1').val(this.value)
-
     const percent = Math.ceil(((this.value - $('#lf-slider-1').attr('min')) / ($('#lf-slider-1').attr('max') - $('#lf-slider-1').attr('min'))) * 100)
-
-    $('#lf-slider-1').css('background', 'linear-gradient(to right, #005aa0 0%, #005aa0 ' + percent + '%, #d9d9d9 ' + percent + '%)')
+    this.style.setProperty('--progress', percent)
   })
 
   $('#lf-slider-3').on('input', function () {
     const percent = Math.ceil(((this.value - this.min) / (this.max - this.min)) * 100)
-    $(this).css('background', 'linear-gradient(to right, #005aa0 0%, #005aa0 ' + percent + '%, #d9d9d9 ' + percent + '%)')
-
+    this.style.setProperty('--progress', percent)
     $('#lf-slider-value-3').val(this.value)
   })
 
   $('#lf-slider-value-3').on('input', function () {
     $('#lf-slider-3').val(this.value)
-
     const percent = Math.ceil(((this.value - $('#lf-slider-3').attr('min')) / ($('#lf-slider-3').attr('max') - $('#lf-slider-3').attr('min'))) * 100)
-
-    $('#lf-slider-3').css('background', 'linear-gradient(to right, #005aa0 0%, #005aa0 ' + percent + '%, #d9d9d9 ' + percent + '%)')
+    this.style.setProperty('--progress', percent)
   })
 
   $('#lf-slider-4').on('input', function () {
     const percent = Math.ceil(((this.value - this.min) / (this.max - this.min)) * 100)
-    $(this).css('background', 'linear-gradient(to right, #005aa0 0%, #005aa0 ' + percent + '%, #d9d9d9 ' + percent + '%)')
-
+    this.style.setProperty('--progress', percent)
     $('#lf-slider-value-4').val(this.value)
   })
+
   $('#lf-slider-value-4').on('input', function () {
     $('#lf-slider-4').val(this.value)
-
     const percent = Math.ceil(((this.value - $('#lf-slider-4').attr('min')) / ($('#lf-slider-4').attr('max') - $('#lf-slider-4').attr('min'))) * 100)
-
-    $('#lf-slider-4').css('background', 'linear-gradient(to right, #005aa0 0%, #005aa0 ' + percent + '%, #d9d9d9 ' + percent + '%)')
+    this.style.setProperty('--progress', percent)
   })
 }
 
