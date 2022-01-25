efa0bc9160b59201292123b6378dafd22999dd75
Carl Törnqvist <carl@tornqv.ist>
2022-01-25 10:47:33 +0000

    Add toggle groups

--- src/js/components/select.js
+++ src/js/components/select.js
@@ -1,35 +1,41 @@
+import $ from 'jquery'
 import 'bootstrap/js/src/dropdown'
 
 const EVENT_PREFIX = /^on/
 const refs = new WeakMap()
 
 /**
+ * @typedef {object} SelectOptions
+ * @property {string} [allLabel] Label for "All" option
+ * @property {boolean} [groupToggle] Enable group toggling
+ * @property {string | RegExp} [smallPattern] RegExp pattern for small text
+ */
+
+/**
  * Decorate select element with custom dropdown
  * @param {Node} element Element to be decorated
- * @param {object} opts Options
- * @property {string} [opts.allLabel] Label for "All" option
- * @property {string | RegExp} [opts.smallPattern] RegExp pattern for small text
+ * @param {SelectOptions} opts Options
  */
-export function select (el, opts) {
-  const config = opts || el.dataset
+export function select (el, opts = el.dataset) {
+  const config = getConfig(opts)
   const select = el.querySelector('.select-options')
   const label = el.querySelector('.select-label')
   const id = Math.random().toString(36).substr(2, 9)
 
   // Don't bother with disabled or missing elements
-  if (!label) throw new Error('Label missing')
-  if (!select) throw new Error('Select missing')
-  if (select.disabled) throw new Error('Select is disabled')
+  if (!label || !select || select.disabled) {
+    if (select?.disabled) el.classList.add('disabled')
+    return
+  }
 
   let preventClose = false
   const initiallySelected = [...select.selectedOptions]
+  const placeholder = initiallySelected.find(isPlaceholder)
   const initiallyValid = initiallySelected.filter((child) => !child.disabled)
-  const smallRegexp = config.smallPattern instanceof RegExp
-    ? config.smallPattern
-    : config.smallPattern ? new RegExp(config.smallPattern) : null
 
   // Set up internal lookup mechanisms
   const buttons = new WeakMap()
+  const groups = new WeakMap()
   const selectAll = new Ref()
   const status = new Ref()
 
@@ -47,19 +53,21 @@ export function select (el, opts) {
       class: 'select-status',
       role: 'status',
       ref: status
-    }, initiallySelected.length
-      ? concatLabels(initiallySelected, smallRegexp)
-      : '')
+    }, initiallyValid.length
+      ? concatLabels(initiallyValid, config.smallPattern)
+      : placeholder
+        ? resolveLabel(placeholder.label, config.smallPattern)
+        : '')
   ])
 
   // The list of options
   const list = h('div', {
     id: `list-${id}`,
     'aria-labelledby': `label-${id}`,
-    class: `select-list ${select.multiple ? 'multiple' : ''} dropdown-menu`,
+    class: 'select-list dropdown-menu',
     onclick () {
       // Disable automatic closing on click
-      preventClose = select.multiple
+      preventClose = preventClose || select.multiple
     }
   }, [
     // Toggle all button
@@ -67,7 +75,7 @@ export function select (el, opts) {
       ? h('button', {
           ref: selectAll,
           type: 'button',
-          class: 'select-option dropdown-item',
+          class: 'select-option multiple dropdown-item',
           onclick (event) {
             const selected = !allAreSelected(select)
             for (const option of select.options) {
@@ -81,33 +89,86 @@ export function select (el, opts) {
     // Proxies for the option/optgroup elements
     ...[...select.children].map(function eachChild (child, index) {
       if (child.nodeName === 'OPTGROUP') {
-        const { label, children } = child
-        return h('fieldset', { class: 'select-group' }, [
-          h('legend', { class: 'select-legend' }, label),
-          ...[...children].map(eachChild)
+        const label = resolveLabel(child.label, config.smallPattern)
+        let children = [...child.children].map(eachChild)
+        const wrapper = new Ref()
+        const button = new Ref()
+        let expanded = true
+
+        // Wrap children in a dropdown menu
+        if (config.groupToggle) {
+          const group = h('fieldset', {
+            'data-target': `#list-${id}`,
+            id: `group-${index}-list-${id}`,
+            class: 'select-list dropdown-menu',
+            'aria-labelledby': `group-${index}-trigger-${id}`
+          }, children)
+
+          // Add to internal lookup table)
+          for (const option of child.children) {
+            groups.set(option, button)
+          }
+
+          // Wrap children
+          children = [group]
+        }
+
+        return h(config.groupToggle ? 'div' : 'fieldset', {
+          ref: wrapper,
+          class: `select-group ${config.groupToggle ? 'show' : ''}`
+        }, [
+          config.groupToggle
+            ? h('button', {
+                ref: button,
+                type: 'button',
+                id: `group-${index}-trigger-${id}`,
+                class: `select-option ${select.multiple ? 'multiple' : ''} toggle dropdown-item`,
+                'aria-expanded': 'true',
+                'aria-controls': `group-${index}-list-${id}`,
+                onclick () {
+                  expanded = !expanded
+                  this.setAttribute('aria-expanded', expanded ? 'true' : 'false')
+                  wrapper.current.classList.toggle('show', expanded)
+
+                  // Disable automatic closing on click
+                  preventClose = true
+
+                  // Ugly hack to prevent Bootstrap dropdown from including
+                  // hidden items in key navigation
+                  for (const option of child.children) {
+                    const button = buttons.get(option)
+                    button.current.classList.toggle('disabled', !expanded)
+                  }
+
+                  // Update dropdown position
+                  $(toggle).dropdown('update')
+                }
+              }, label)
+            : h('legend', { class: 'select-legend' }, label),
+          ...children
         ])
       }
 
-      const { label, disabled, selected, value } = child
-
       // Don't include the typical leading placeholder option
       // E.g. <option value="" disabled selected>Choose something</option>
-      if (!index && selected && disabled && !value) return null
+      if (isPlaceholder(child, index)) return null
+
+      const { label, disabled, selected } = child
 
       // Add to internal element reverse lookup table
-      const ref = new Ref()
-      buttons.set(child, ref)
+      const button = new Ref()
+      buttons.set(child, button)
 
       return h('button', {
-        ref,
         disabled,
+        ref: button,
         type: 'button',
-        class: `select-option dropdown-item ${selected ? 'selected' : ''}`,
+        class: `select-option dropdown-item ${select.multiple ? 'multiple' : ''} ${selected ? 'selected' : ''}`,
         onclick (event) {
           child.selected = select.multiple ? !child.selected : true
           select.dispatchEvent(new window.Event('change'))
         }
-      }, resolveLabel(label, smallRegexp))
+      }, resolveLabel(label, config.smallPattern, disabled ? '' : undefined))
     })
   ])
 
@@ -123,11 +184,36 @@ export function select (el, opts) {
   select.addEventListener('change', function () {
     selectAll.current?.classList.toggle('selected', allAreSelected(select))
 
+    const states = new Map()
+
     // Update proxy buttons
     for (const option of select.options) {
       if (option.disabled) continue
       const ref = buttons.get(option)
       ref?.current.classList.toggle('selected', option.selected)
+
+      // Calculate group status
+      if (groups.has(option)) {
+        const button = groups.get(option)
+        let current = states.get(button)
+
+        if (option.selected) {
+          if (!current) current = 'selected'
+          else if (current === 'unselected') current = 'indeterminate'
+        } else if (current === 'selected') {
+          current = 'indeterminate'
+        } else if (!current) {
+          current = 'unselected'
+        }
+
+        states.set(button, current)
+      }
+    }
+
+    // Update group status
+    for (const [button, state] of states) {
+      button.current.classList.toggle('selected', state === 'selected')
+      button.current.classList.toggle('indeterminate', state === 'indeterminate')
     }
 
     const selected = [
@@ -139,9 +225,11 @@ export function select (el, opts) {
     statusNode.dataset.count = selected.length || ''
 
     // Update status label
-    const items = selected.length ? selected : initiallySelected
+    const items = selected.length
+      ? selected
+      : placeholder ? [placeholder] : []
     const [first, ...rest] = statusNode.childNodes
-    const next = concatLabels(items.length ? items : [''], smallRegexp)
+    const next = items.length ? concatLabels(items, config.smallPattern) : ['']
     for (const child of rest) child.remove()
     first.replaceWith(...next)
   })
@@ -153,6 +241,33 @@ export function select (el, opts) {
 }
 
 /**
+ * @param {SelectOptions} options
+ * @returns {SelectOptions}
+ */
+function getConfig (options) {
+  let { smallPattern, allLabel, groupToggle } = options
+
+  if (smallPattern && !(smallPattern instanceof RegExp)) {
+    smallPattern = new RegExp(smallPattern)
+  }
+
+  if (typeof groupToggle !== 'undefined') {
+    groupToggle = groupToggle !== false
+  }
+
+  return { smallPattern, allLabel, groupToggle }
+}
+
+/**
+ * @param {Node} option
+ * @returns {boolean}
+ */
+function isPlaceholder (option) {
+  const { value, disabled, hidden } = option
+  return !value && disabled && hidden
+}
+
+/**
  * @param {Node} select
  * @returns {boolean}
  */
@@ -161,7 +276,7 @@ function allAreSelected (select) {
 }
 
 /**
- * @param {string[]} labels
+ * @param {Node[]} labels
  * @param {RegExp} regexp
  * @returns {(string | Node)[]}
  */
@@ -178,12 +293,12 @@ function concatLabels (labels, regexp) {
  * @param {RegExp} regexp
  * @returns {(string | Node)[]}
  */
-function resolveLabel (label, regexp) {
+function resolveLabel (label, regexp, className = 'text-muted') {
   const match = label.match(regexp)
   if (!match) return [label]
   return [
     label.replace(regexp, ''),
-    h('small', { class: 'text-muted' }, match[1] || match[0])
+    h('small', { class: className }, match[1] || match[0])
   ]
 }
 
