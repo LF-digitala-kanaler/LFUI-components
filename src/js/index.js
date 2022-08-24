daa1262253b4f3b524aa315213e437ac11ec9de6
Carl Törnqvist <carl@tornqv.ist>
2022-08-24 16:03:59 +0200

    Normalize file names

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,13 @@
-/* eslint-disable import/first */
 import './components/utilities'
 import './components/alert'
 import './components/buttons'
 import './components/collapse'
 import './components/custom-file'
 import './components/dropdown'
-import './components/datepicker'
 import './components/focus'
 import './components/modal'
 import './components/tabs'
 import './components/tables'
-import './components/timeline'
-/* eslint-enable import/first */
 
 export { select } from './components/select'
+export { datepicker } from './components/datepicker'
