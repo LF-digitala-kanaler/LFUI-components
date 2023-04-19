c0e356d7c734284fa4daa254565c8164fc8bf521
Magnus Fredlundh <magnus.fredlundh@lansforsakringar.se>
2023-04-19 09:40:29 +0200

    add test for body text

---
+++ tests/body-text.spec.js
@@ -0,0 +1,6 @@
+import { visualTest } from '../tools/test.js'
+
+const section = 'typography-text'
+const name = 'body-text'
+
+visualTest(`${section}-${name}--paragraph`, { title: `${name}--paragraph` })
