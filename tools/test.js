4435e939465086e6258df38b5ffc14093582ac9d
Magnus Fredlundh <magnus.fredlundh@lansforsakringar.se>
2023-06-05 15:36:26 +0200

    fix: make test wait a bit

--- tools/test.js
+++ tools/test.js
@@ -5,6 +5,8 @@ const url = '/iframe.html?id='
 export async function visualTest(variant, { title = {}, options = undefined } = {}) {
   test(title || variant, async ({ page }) => {
     await page.goto(url + variant)
-    await expect(page).toHaveScreenshot(options || { fullPage: true })
+    await page
+      .waitForSelector('body')
+      .then(async () => await expect(page).toHaveScreenshot(options || { fullPage: true }))
   })
 }
