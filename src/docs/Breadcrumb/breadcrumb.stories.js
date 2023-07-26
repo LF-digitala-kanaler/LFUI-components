eccc600cefa281fe4dde039988bf78073b950713
Magnus Fredlundh <magnus.fredlundh@lansforsakringar.se>
2023-07-26 09:17:29 +0200

    Breadcrumb documentation

--- src/docs/Breadcrumb/breadcrumb.stories.js
+++ src/docs/Breadcrumb/breadcrumb.stories.js
@@ -2,7 +2,36 @@ import Breadcrumb from './breadcrumb.html?raw'
 
 export default {
   title: 'Navigations/Breadcrumb',
-  parameters: { backgrounds: { default: 'gray' } }
+  parameters: {
+    backgrounds: { default: 'grey' },
+    badges: ['stable']
+  }
 }
 
-export const breadcrumb = () => Breadcrumb
+function Template({ transparent }) {
+  return `
+  <nav aria-label="Breadcrumb">
+    <ol class="breadcrumb ${transparent ? 'breadcrumb-transparent' : ''}">
+      <li class="breadcrumb-item">
+        <a href="#" onclick="return false">Private banking</a>
+      </li>
+      <li class="breadcrumb-item">
+        <a href="#" onclick="return false">Insurances</a>
+      </li>
+      <li class="breadcrumb-item active" aria-current="page">Car insurance</li>
+    </ol>
+  </nav>
+  `
+}
+
+export const Default = Template.bind({})
+Default.args = {
+  transparent: false
+}
+
+export const Transparent = Template.bind({})
+Transparent.args = {
+  transparent: true
+}
+
+export const breadcrumbExample = () => Breadcrumb
