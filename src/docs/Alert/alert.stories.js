9fa41c0bb68a643607452413cb5d147af7eaf502
Magnus Fredlundh <magnus.fredlundh@lansforsakringar.se>
2024-04-29 14:26:25 +0200

    Next version (#186)
    * fix: storybook dependencies (#184)
    * fix: contrast on placeholder text in input fields
    * fix: contrast on checkbox and radio buttons
    * fix: show focus ring on button groups
    * fix: show focus ring around checkbox/radio and label
    * fix: update alert component (#190)
    * fix:update alert component
    * fix(alert): a11y improvements
    
    * feat:ally update (#185)
    * feat: add infobox component (#183)
    * Feature/dropdown-a11y (#180)
    
    * doc: FormGroup
    * doc: grouping of checkboxes and radio buttons
    * doc: fix typo in checkbox docs

--- src/docs/Alert/alert.stories.js
+++ src/docs/Alert/alert.stories.js
@@ -2,18 +2,21 @@ import BannerRaw from './banner.html?raw'
 import WithIconRaw from './withIcon.html?raw'
 import DefaultRaw from './default.html?raw'
 import WithButtonRaw from './withButton.html?raw'
-import WithShadowRaw from './withShadow.html?raw'
 import WithCloseButtonRaw from './withCloseButton.html?raw'
 import WithStrongEmphasisRaw from './withStrongEmphasis.html?raw'
+import DynamicRaw from './dynamic.html?raw'
 
 export default {
-  title: 'System display/Alert'
+  title: 'System display/Alert',
+  parameters: {
+    badges: ['stable']
+  }
 }
 
 export const Defaults = () => DefaultRaw
 export const WithIcon = () => WithIconRaw
 export const WithButton = () => WithButtonRaw
-export const WithShadow = () => WithShadowRaw
 export const WithCloseButton = () => WithCloseButtonRaw
 export const WithStrongEmphasis = () => WithStrongEmphasisRaw
 export const Banner = () => BannerRaw
+export const Dynamic = () => DynamicRaw
